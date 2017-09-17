d34289f68f178e5fa9ec8c80c7b2cd5c82469f75
ragnar321 <github@ragnar.neomailbox.ch>
2017-09-17 13:26:14 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aws_status',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/ragnar321/aws_status/',
+    license='GPL v2',
+    author='ragnar',
+    author_email='github@ragnar.neomailbox.ch',
+    description='AWS status checker for North Virginia'
+)
