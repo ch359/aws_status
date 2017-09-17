5d9984e5bc1430418a34ea8276173e3a158c281d
ragnar321 <github@ragnar.neomailbox.ch>
2017-09-17 13:15:39 +0100

    Accounts for all N Virginia occurrences, not just the first. More sophisticated error printing.

--- aws_status.py
+++ aws_status.py
@@ -1,5 +1,9 @@
+import sys
+import time
+
 import requests
 from bs4 import BeautifulSoup
+from termcolor import colored
 
 """Alert when North Virginia server on AWS has a status other than nominal."""
 
@@ -11,24 +15,28 @@ def scraping():
     body = soup.contents[2]
     body = body.find(id='NA_block').get_text()
     body = body.split(sep="\n")
-    for b in body:
-        if b == 'Amazon API Gateway (N. Virginia)':
-            index = body.index(b)
-            server = body[index]
-            status = body[index + 1]
-            return server, status
-    if 'Amazon API Gateway (N. Virginia)' not in body:
-        print('Something has gone horribly wrong; N. Virginia does not appear to be available. Perhaps the format'
-              'of the page has changed?')
+    status = [(body[body.index(b)], body[body.index(b) + 1]) for b in body if '(N. Virginia)' in b]
+    return status
 
 
-def alert(arg):
+def alert(status):
     """Taking the scraped page as input, print an alert if status is anything other than nominal."""
-    server, status = arg
-    if status != 'Service is operating normally':
-        print(server, ' - Shit be going down! The detailed status is: ', status)
-    else:
-        print(server, ' - ', status)
+    if not status:
+        sys.exit("Cannot find N. Virginia status. Perhaps the page format has changed?")
+    errors = [row for row in status if 'Service is operating normally' not in row[1]]
+    if not errors:
+        print('\n', time.strftime('%H:%M:%S'), 'All services are operating normally.')
+    elif errors:
+        print('\n', colored(time.strftime('%H:%M:%S'), 'red'), colored('Errors have been detected with the following '
+                                                                       'services: ', 'red'))
+        for row in errors:
+            print(' ', row[0], ' - ', row[1])
 
 
-alert(scraping())
+while True:
+    alert(scraping())
+    time.sleep(20)
+
+# soup = BeautifulSoup(open('aws.htm'), 'html.parser') <-- This code can be used to test against a local and editable
+# copy of the page to simulate failing services. Comment out the requests.get line above to avoid pointlessly calling
+# the live page.
